6b53c012e07674e811a7707eeee1a54707f6336c
patriciaSR <patricia.suarez.psr@gmail.com>
2019-08-21 19:16:12 +0200

    add flip functions for play and add counter available turns

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,7 +3,8 @@
 const startButton = document.querySelector('.game__button');
 const cardList = document.querySelector('.card__list');
 
-
+let availableFlips = 2;
+let previousCardElement;
 
 const ENDPOINT = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
 
@@ -13,7 +14,7 @@ setDefaultNumber(localStorage.getItem('cardNumber'));
 function setDefaultNumber(number) {
   const radios = document.querySelectorAll('input[type="radio"]');
   for (const radio of radios) {
-    if(number === radio.value) {
+    if (number === radio.value) {
       radio.checked = true;
     }
   }
@@ -54,13 +55,51 @@ function printCard(pair, image, name) {
   newLi.appendChild(newFrontImage);
   newLi.appendChild(backImage);
 
-  newLi.addEventListener('click', flip);
+  newLi.addEventListener('click', turn);
 
   cardList.appendChild(newLi);
 }
 
-function flip(event) {
-  const eventLi = event.currentTarget;
+function turn(event) {
+  const currentCard = event.currentTarget;
+  if (canFlipCard(currentCard) && availableFlips) {
+    flipCard(currentCard);
+    availableFlips--;
+
+    if (!canFlipOtherCard()) {
+      const cardsAreEqual = checkCards(previousCardElement, currentCard);
+      if (cardsAreEqual) {
+        //SI HAY MÁS CARTAS SIN CARD--FLIPPED PUEDES SEGUIR JUGANDO. SI NO, MENSAJE DE FIN DE JUEGO.
+        // TODO
+      } else {
+        //TEMPORIZADOR PARA QUE SE GIREN LAS CARTAS
+        flipCard(previousCardElement);
+        flipCard(currentCard);
+      }
+      availableFlips = 2;
+    }
+
+    previousCardElement = currentCard;
+  }
+}
+
+function checkCards(previousCardElement, actualCard) {
+  const previousPair = previousCardElement.getAttribute('data-pair');
+  const actualPair = actualCard.getAttribute('data-pair');
+
+  return previousPair === actualPair;
+}
+
+function canFlipCard(eventLi) {
+  const eventImg = eventLi.querySelector('.card__front-img');
+  return !eventImg.classList.contains('card--flipped');
+}
+
+function flipCard(eventLi) {
   const eventImg = eventLi.querySelector('.card__front-img');
   eventImg.classList.toggle('card--flipped');
+}
+
+function canFlipOtherCard() {
+  return availableFlips > 0;
 }
