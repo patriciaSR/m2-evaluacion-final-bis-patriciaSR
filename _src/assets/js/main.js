2493648ac7cdd2906572abed9996136f5a9ec82a
patriciaSR <patricia.suarez.psr@gmail.com>
2019-08-21 17:38:35 +0200

    add first main.css styles with position absolute to imgs card

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -3,6 +3,8 @@
 const startButton = document.querySelector('.game__button');
 const cardList = document.querySelector('.card__list');
 
+
+
 const ENDPOINT = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
 
 startButton.addEventListener('click', startGame);
@@ -29,10 +31,16 @@ function printCard(pair, image, name) {
   const newLi = document.createElement('li');
   newLi.classList.add('card__item');
   newLi.setAttribute('data-pair', pair);
-  const newImage = document.createElement('img');
-  newLi.classList.add('card__img');
-  newImage.alt = name;
-  newImage.src = image;
-  newLi.appendChild(newImage);
+  const newFrontImage = document.createElement('img');
+  newFrontImage.classList.add('card__front-img');
+  newFrontImage.alt = name;
+  newFrontImage.src = image;
+  const backImage = document.createElement('img');
+  backImage.classList.add('card__back-img');
+  backImage.alt = 'Adalab card';
+  backImage.src = 'assets/images/adalab.png';
+
+  newLi.appendChild(newFrontImage);
+  newLi.appendChild(backImage);
   cardList.appendChild(newLi);
 }
